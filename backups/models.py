44e530b97bcfc54a362aa11e0b84bb297dfe0b41
bvrti <74336093+bvrti@users.noreply.github.com>
2022-09-01 13:51:03 +0200

    request model

--- backups/models.py
+++ backups/models.py
@@ -1,5 +1,5 @@
 from djongo import models
-
+from datetime import datetime
 #CHOICES
 AREA = (
     ( 'SMT' , 'SMT'),
@@ -18,3 +18,8 @@ class machine(models.Model):
     machine_fisname = models.CharField(max_length=24)
     machine_area = models.CharField(choices=AREA, max_length=10)
     owner = models.CharField(max_length=100)
+
+class requestBackup(models.Model):
+    requestBackup_holistech = models.ForeignKey(machine, on_delete=models.CASCADE)
+    requestBackup_reason = models.CharField(max_length=250)
+    requestBackup_date = models.DateTimeField(default=datetime.now(), blank=True)
