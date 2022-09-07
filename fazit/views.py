83ec6851feebf0ee8953c707fba98108dc0a5103
bvrti <74336093+bvrti@users.noreply.github.com>
2022-09-07 18:00:08 +0200

    fazit -> audi

--- fazit/views.py
+++ fazit/views.py
@@ -14,20 +14,25 @@ def fazit(request):
     if request.POST:
         if form.is_valid():
             host = '10.237.135.13'
-            user = 'jj00sp'
-            passwd = ''
-            src = "/fis/mantis/data/FAZIT/save/"
-            dst = "/fis/mantis/data/FAZIT/tmp/"
+            user = 'django'
+            passwd = 'djangoapps!@21'
             fazits = form['fazits'].value()
             ssh = paramiko.SSHClient()
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(hostname=host, username=user,
                         password=passwd, look_for_keys=False)
             for line in fazits.splitlines():
-                DPZpattern = re.compile("[D][P][Z][-][0-9A-Z]{5}[.][0-9]{2}[.][0-9]{2}[U][P][S][0-9]{5}")
-                if DPZpattern.match(line):
+                VWpattern = re.compile("[D][P][Z][-][0-9A-Z]{5}[.][0-9]{2}[.][0-9]{2}[U][P][S][0-9]{5}")
+                AUDIpattern = re.compile("[D][P][Z][-][0-9A-Z]{5}[.][0-9]{2}[.][0-9]{2}[U][P][0-9]{6}")
+                if VWpattern.match(line):
+                    src = "/fis/mantis/data/FAZIT/save/"
+                    dst = "/fis/mantis/data/FAZIT/tmp/"
                     command = "mv " + str(src) + line + "_txt" + " " + dst
-                    print(command)
+                    (stdin, stdout, stderr) = ssh.exec_command(command)
+                elif AUDIpattern.match(line):
+                    src = "/fis/mantis/data/FAZIT_XML/save/"
+                    dst = "/fis/mantis/data/FAZIT_XML/tmp/"
+                    command = "mv " + str(src) + line + "_txt" + " " + dst
                     (stdin, stdout, stderr) = ssh.exec_command(command)
                 else:
                     print("FAZIT string is not valid")
--- main/static/js/index.js
+++ main/static/js/index.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const bakcup = document.getElementById('backup');
+    const fazit = document.getElementById('fazit');
     const header = document.getElementById('header-text');
     bakcup.addEventListener('mouseover', () => {
         header.innerHTML = 'Backup Monitor'
     });
+    
     bakcup.addEventListener('mouseout', () => {
+        header.innerHTML = 'PDS Apps'
+    });
+
+    fazit.addEventListener('mouseover', () => {
+        header.innerHTML = 'FAZIT'
+    });
+
+    fazit.addEventListener('mouseout', () => {
         header.innerHTML = 'PDS Apps'
     });
 });
